7f5f22069f238e0c1a4cf81dbe76213310cbddee
root <vaibhav.pathak@cuelogic.com>
2018-10-01 18:50:49 +0530

    add new external and inline styling and reflect over jsx files

--- src/App.js
+++ src/App.js
@@ -38,11 +38,22 @@ class App extends Component {
   }
 
   render() {
+    //Inline styling css:
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hello World...!</h1>
         <p>This is first React App.</p>
-        <button onClick={() => this.switchNameHandler('ArrowMax')}>Switch Name</button>
+        <button 
+        style={style}
+        onClick={() => this.switchNameHandler('ArrowMax')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age} />
